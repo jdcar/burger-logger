4fa69cc29bba4a4583368da8325c80dbce9e7b2c
jdcar <jdcarlstone@gmail.com>
2021-02-07 13:22:21 -0600

    Setup the db and config folder

---
+++ config/connection.js
@@ -0,0 +1,15 @@
+// connect mysql
+const mysql = require('mysql')
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'burgers_db',
+    password: 'root',
+});
+connection.connect(err => {
+    if (err) throw err
+    console.log(`MySQL connected on ${connection.threadId}`)
+});
+
+module.exports = connection

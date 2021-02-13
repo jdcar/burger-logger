ca74d7574712aa5bebd04cd0191bf320162b8673
jdcar <jdcarlstone@gmail.com>
2021-02-12 20:36:17 -0600

    Add if statements to handlebars

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,5 @@
 INSERT INTO burgers (burger_name, devoured) VALUES ("Swiss", true);
 INSERT INTO burgers (burger_name, devoured) VALUES ("Cheddar", true);
 INSERT INTO burgers (burger_name, devoured) VALUES ("American", true);
+
+UPDATE burgers SET devoured= false WHERE burger_name = "Cheddar";

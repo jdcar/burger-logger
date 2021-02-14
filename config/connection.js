e7d8c7f1956170c69f0f150740c59c136fb545e0
jdcar <jdcarlstone@gmail.com>
2021-02-14 11:22:28 -0600

    Heroku setup

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,25 @@
 // connect mysql
 const mysql = require('mysql')
+const connection;
 
-var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    database: 'burgers_db',
-    password: 'root',
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else {
+    connection = mysql.createConnection({
+        host: 'qz8si2yulh3i7gl3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        user: 'gbk3xj2bi7veu730',
+        password: 'trwe29vdq4rvc7k6',
+        database: 'eivafy114l2758wf'
+    })
+}
+}
+
+// var connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     database: 'burgers_db',
+//     password: 'root',
+// });
 connection.connect(err => {
     if (err) throw err
     // console.log(`MySQL connected on ${connection.threadId}`)
